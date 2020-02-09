7d43a5e2fe0ed6e259d5b14a7aa45487da1e66fd
David Medina <davidmpuyol@gmail.com>
2020-02-09 14:54:38 +0100

    .

---
+++ precache-manifest.6373df4fd6c9bd7d6e3ad98f4a983791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "784e321d0cfbcbaea01c",
+    "url": "css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "214c6dac4e6f606369e593e112661eb5",
+    "url": "index.html"
+  },
+  {
+    "revision": "784e321d0cfbcbaea01c",
+    "url": "js/app.d73e966c.js"
+  },
+  {
+    "revision": "083f825bd7b63ea74c70",
+    "url": "js/chunk-vendors.5006c987.js"
+  },
+  {
+    "revision": "b0d6ded906db037aaed0ff85b3eba813",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
